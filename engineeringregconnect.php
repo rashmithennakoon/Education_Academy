d0e31ad95af035d4f82b5f72c8bcd6ca9e7bcb28
Rashmi Thennakoon <151599524+rashmithennakoon@users.noreply.github.com>
2024-01-06 14:29:45 +0530

    Add files via upload

---
+++ engineeringregconnect.php
@@ -0,0 +1,23 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+        $conn = mysqli_connect('localhost', 'root', '', 'wisdomewise_academy') or die("Connection Failed:" . mysqli_connect_error());
+        if (isset($_POST['name']) && isset($_POST['id']) && isset($_POST['dob']) && isset($_POST['email']) && isset($_POST['address']) && isset($_POST['phone']) && isset($_POST['degree']) && isset($_POST['pict'])) {
+            $name = $_POST['name'];
+            $id = $_POST['id'];
+            $dob = $_POST['dob'];
+            $email = $_POST['email'];
+            $address = $_POST['address'];
+            $phone = $_POST['phone'];
+            $degree = $_POST['degree'];
+            $pict = $_POST['pict'];
+            
+            $sql = "INSERT INTO `engineering` (`Full_Name`, `ID`, `DOB`, `Email`, `Address`, `Telephone_Number`,`Degree`, `Payment_Slip_Link`) VALUES ('$name','$id','$dob','$email', '$address', '$phone','$degree', '$pict')";
+            $query = mysqli_query($conn, $sql);
+            if ($query) {
+                header("Location: success.php");
+            } else {
+                echo 'Error Occurred: ' . mysqli_error($conn);
+            }
+        }
+    }
+?>
